3d6e29032cc563eabafe591e90486e4d92a2df6f
Chris Pangburn <cpangburn@gmail.com>
2021-09-06 11:07:19 +0100

    DS18b20 test

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const bme280 = require('bme280');
+let ds18b20 = require('ds18b20');
+
+ds18b20.sensors(function(err, ids) {
+  // got sensor IDs ...
+  console.log(ids);
+});
 
 bme280.open({ i2cAddress: 0x76 }).then(async sensor => {
   console.log(await sensor.read());
