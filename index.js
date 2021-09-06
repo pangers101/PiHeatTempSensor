c370d2d47b719ae38b6e1fbdeaa5a2be6949bddb
Chris Pangburn <cpangburn@gmail.com>
2021-09-06 11:26:49 +0100

    Sensor reading updates

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const bme280 = require('bme280');
 let ds18b20 = require('ds18b20');
 
-ds18b20.sensors(function(err, ids) {
-  // got sensor IDs ...
-  console.log(ids);
+let sensors = [
+  {
+    id: '28-0517c1fe23ff', 
+    label: 'Outside',
+    type: 'ds18b20'
+  }, 
+  {
+    id: '28-05179039bfff', 
+    label: 'Lounge',
+    type: 'ds18b20'
+  },
+  {
+    type: 'bme280',
+    i2cAddress: 0x76,
+    pressure: true,
+    temperature: false 
+  }
+];
+
+sensors.forEach((sensorObj) => {
+  switch (sensorObj.type){
+    case 'ds18b20':
+      sensorObj.currentTemp = ds18b20.temperatureSync(sensorObj.id);
+      break;
+    case 'bme280':
+
+  }
 });
 
-bme280.open({ i2cAddress: 0x76 }).then(async sensor => {
-  console.log(await sensor.read());
-  await sensor.close();
-}).catch(console.log);
+console.log(sensors);
