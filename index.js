35ea2257e02a0d11a3bc822c78a0effa04802f65
Chris Pangburn <cpangburn@gmail.com>
2021-09-06 15:18:16 +0100

    test set interval update

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 let updateSensors = require('./lib/updateSensors.js');
 let sensors;
 
-updateSensors()
-  .then(sensArray => {
-    console.log('SENSORS UPDATED:');
-    console.log(sensArray);
-  })
-  .catch(e => {
-    console.log(e);
-    //throw new Error(e);
-  });
+let updateInterval = setInterval(() => {
+  updateSensors()
+    .then(sensArray => {
+      console.log('SENSORS UPDATED:');
+      console.log(JSON.stringify(sensArray));
+    })
+    .catch(e => {
+      console.log(e);
+      //throw new Error(e);
+    });
+}, 5000);
