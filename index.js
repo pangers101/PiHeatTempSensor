20f187133cece1af74411149d4f94e7872c70644
Chris Pangburn <cpangburn@gmail.com>
2021-09-06 15:30:26 +0100

    Changed function to async

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
+'use strict';
 let updateSensors = require('./lib/updateSensors.js');
-let sensors;
+let sensors, sensArray, newSensString, currentSensString;
 
-let updateInterval = setInterval(() => {
-  updateSensors()
-    .then(sensArray => {
-      console.log('SENSORS UPDATED:');
-      console.log(JSON.stringify(sensArray));
-    })
-    .catch(e => {
-      console.log(e);
-      //throw new Error(e);
-    });
+let updateInterval = setInterval(async () => {
+  try{
+    let sensArray = await updateSensors();
+    console.log('CHECKING SENSORS...');
+    newSensString = JSON.stringify(sensArray);
+    if(newSensString !== currentSensString){
+      console.log('SENSORS UPDATED');
+      console.log(newSensString);
+      //Send out SSE
+      currentSensString = newSensString;
+    }else{
+      console.log('SENSORS THE SAME, DO NOTHING')
+    }
+  }catch(e){
+    console.log(e);
+    //throw new Error(e);
+  }
 }, 10000);
