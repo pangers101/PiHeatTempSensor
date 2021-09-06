eb6318e6e3634cbc6680f5400c67c2e6bb9366f9
Chris Pangburn <cpangburn@gmail.com>
2021-09-06 15:13:35 +0100

    Promisifying update of sensors

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 let updateSensors = require('./lib/updateSensors.js');
+let sensors;
 
-updateSensors();
+updateSensors()
+  .then(sensArray => {
+    console.log('SENSORS UPDATED:');
+    console.log(sensArray);
+  })
+  .catch(e => {
+    console.log(e);
+    //throw new Error(e);
+  });
