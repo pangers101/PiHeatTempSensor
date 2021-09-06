bebd5940e6a75d6b87d5a66a54e171e1be7a0273
Chris Pangburn <cpangburn@gmail.com>
2021-09-06 12:28:24 +0100

    Async IEF

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 const readBme280 = require('./bme280');
 let ds18b20 = require('ds18b20');
+async () => {
+  let sensors = [
+    {
+      id: '28-0517c1fe23ff', 
+      label: 'Outside',
+      type: 'ds18b20'
+    }, 
+    {
+      id: '28-05179039bfff', 
+      label: 'Lounge',
+      type: 'ds18b20'
+    },
+    {
+      type: 'bme280',
+      i2cAddress: 0x76,
+      pressure: true,
+      temperature: false 
+    }
+  ];
 
-let sensors = [
-  {
-    id: '28-0517c1fe23ff', 
-    label: 'Outside',
-    type: 'ds18b20'
-  }, 
-  {
-    id: '28-05179039bfff', 
-    label: 'Lounge',
-    type: 'ds18b20'
-  },
-  {
-    type: 'bme280',
-    i2cAddress: 0x76,
-    pressure: true,
-    temperature: false 
-  }
-];
+  await sensors.forEach(async (sensorObj) => {
+    switch (sensorObj.type){
+      case 'ds18b20':
+        sensorObj.currentTemp = ds18b20.temperatureSync(sensorObj.id);
+        break;
+      case 'bme280':
+        sensorObj.data = await readBme280(sensorObj.i2cAddress);
+        break;
+    }
+  });
 
-sensors.forEach(async (sensorObj) => {
-  switch (sensorObj.type){
-    case 'ds18b20':
-      sensorObj.currentTemp = ds18b20.temperatureSync(sensorObj.id);
-      break;
-    case 'bme280':
-      sensorObj.data = await readBme280(sensorObj.i2cAddress);
-      break;
-  }
-});
-
-console.log(sensors);
+  console.log(sensors);
 
 
-
+}
