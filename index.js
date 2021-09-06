b5356fb7bb415dfea36f12687ddb9f65ceb30fa8
Chris Pangburn <cpangburn@gmail.com>
2021-09-06 20:58:25 +0100

    Stringify

--- index.js
+++ index.js
@@ -1,48 +1,45 @@
 'use strict';
 
+let express = require('express');
+let app = express();
+let SSE = require('express-sse');
 
-  let express = require('express');
-  let app = express();
-  let SSE = require('express-sse');
+let updateSensors = require('./lib/updateSensors.js');
+let sensors, sensArray, newSensString, currentSensString, sse;
 
-  let updateSensors = require('./lib/updateSensors.js');
-  let sensors, sensArray, newSensString, currentSensString, sse;
+app.get('/getsensors', async (req, res, next) => {
+  let showSensors = JSON.stringify(await updateSensors());
+  res.json(showSensors);
+});
 
-  
+(async () => {
+  let initialData = await updateSensors();
+  sse = new SSE(JSON.stringify(initialData));
+  app.get('/sensorstream', sse.init);
+})();
 
-  app.get('/getsensors', async (req, res, next) => {
-    let showSensors = await updateSensors();
-    res.json(showSensors);
-  });
+let updateInterval = setInterval(getSensors, 10000);
 
-  (async () => {
-    let initialData = await updateSensors();
-    sse = new SSE(JSON.stringify(initialData));
-    app.get('/sensorstream', sse.init);
-  })();
-  
-  let updateInterval = setInterval(getSensors, 10000);
+app.listen(11013, (req, res) => {
+  console.log('app started listening on 11013');
+});
 
-  app.listen(11013, (req, res) => {
-    console.log('app started listening on 11013');
-  });
-
-  async function getSensors(){
-    try{
-      let sensArray = await updateSensors();
-      console.log('CHECKING SENSORS...');
-      newSensString = JSON.stringify(sensArray);
-      if(newSensString !== currentSensString){
-        console.log('SENSORS UPDATED');
-        console.log(newSensString);
-        //Send out SSE
-        sse.send(newSensString);
-        currentSensString = newSensString;
-      }else{
-        console.log('SENSORS THE SAME, DO NOTHING')
-      }
-    }catch(e){
-      console.log(e);
-      //throw new Error(e);
+async function getSensors(){
+  try{
+    let sensArray = await updateSensors();
+    console.log('CHECKING SENSORS...');
+    newSensString = JSON.stringify(sensArray);
+    if(newSensString !== currentSensString){
+      console.log('SENSORS UPDATED');
+      console.log(newSensString);
+      //Send out SSE
+      sse.send(newSensString);
+      currentSensString = newSensString;
+    }else{
+      console.log('SENSORS THE SAME, DO NOTHING')
     }
+  }catch(e){
+    console.log(e);
+    //throw new Error(e);
   }
+}
