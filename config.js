972190782be5be93c5a82151aa7fce5012cc1d71
Chris Pangburn <cpangburn@gmail.com>
2021-09-10 14:28:47 +0100

    Thingspeak fields

--- config.js
+++ config.js
@@ -2,18 +2,21 @@ exports.sensors = [
     {
       id: '28-0517c1fe23ff', 
       label: 'Outside',
-      type: 'ds18b20'
+      type: 'ds18b20',
+      thingspeak_field: 4
     }, 
     {
       id: '28-05179039bfff', 
       label: 'Lounge',
-      type: 'ds18b20'
+      type: 'ds18b20',
+      thingspeak_field: 3
     },
     {
       type: 'bme280',
       label: 'Pressure',
       i2cAddress: 0x76,
       pressure: true,
-      temperature: false 
+      temperature: false,
+      thingspeak_field: 5
     }
   ];
